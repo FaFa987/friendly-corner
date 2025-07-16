dce03be9c1e0fababdb1d28cf801be5b918d0e52
Fatemeh <f.khalleghi.777@gmail.com>
2025-07-16 18:55:36 +0200

    switch between components inside the same page

--- FrontEnd/src/Components/User/UserSidebar.jsx
+++ FrontEnd/src/Components/User/UserSidebar.jsx
@@ -1,14 +1,13 @@
 import './UserPage.css';
-import { Link } from 'react-router-dom';
 
 function UserSidebar({ setSelectedComponent }) {
     return (
         <div className="user-sidebar"> 
             <button onClick={() => setSelectedComponent('SlackFeed')} className="user-link ">SlackFeed</button>
             <button onClick={() => setSelectedComponent('MeetingRoomCalendar')} className="user-link ">BookRoom</button>
-            <Link to="/spotify-player" className="user-link" style={{ textAlign: 'left', display: 'block' }}>
+            <button onClick={() => setSelectedComponent('SpotifyPlayer')} className="user-link">
                 <span role="img" aria-label="music">🎵</span> PlayMusic
-            </Link>
+            </button>
         </div> 
     ); 
 }
