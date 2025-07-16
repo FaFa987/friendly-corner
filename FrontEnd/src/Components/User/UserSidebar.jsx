077fa06cef1bae980852fb49db185f7bf7b1bc34
Fatemeh <f.khalleghi.777@gmail.com>
2025-07-16 18:53:35 +0200

    sidebar link to PlayMusic button

--- FrontEnd/src/Components/User/UserSidebar.jsx
+++ FrontEnd/src/Components/User/UserSidebar.jsx
@@ -1,12 +1,16 @@
 import './UserPage.css';
+import { Link } from 'react-router-dom';
+
 function UserSidebar({ setSelectedComponent }) {
-    
     return (
         <div className="user-sidebar"> 
             <button onClick={() => setSelectedComponent('SlackFeed')} className="user-link ">SlackFeed</button>
             <button onClick={() => setSelectedComponent('MeetingRoomCalendar')} className="user-link ">BookRoom</button>
+            <Link to="/spotify-player" className="user-link" style={{ textAlign: 'left', display: 'block' }}>
+                <span role="img" aria-label="music">🎵</span> PlayMusic
+            </Link>
         </div> 
     ); 
-};
+}
 
 export default UserSidebar;
